8b09e00af97109853d2f6133b8b4a2ff9212d1a6
Demonbek <34240090+Demonbek@users.noreply.github.com>
2020-02-07 19:23:51 +0300

    Add files via upload

---
+++ burgery.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" charset="utf-8"/>
+	<meta name="DemonApps" content="Demon" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+	<title>Бургеры, Шаурма...</title>
+</head>
+
+<body>
+<div id="block-body">
+<?php
+	include("include/block-header.php");
+?>
+
+<div id="block-content">
+<div id="text">
+<h1 id="zagolovok">Папа Карло</h1>
+
+<p>Наши блюда мы готовим в дровяной печи,
+что позволяет делать любое блюдо
+совершенно особенным!</p>
+
+<p>Пицца, гамбургеры, хлеб получаются 
+равномерно пропеченными. Прогрев до
+высоких температур дает возможность
+готовить блюда с хрустящей корочкой.
+Тесто быстро «прихватывается»,
+становиться румяным и хрустящим.</p>
+
+<p>Приготовленные в печи блюда имеют
+особенный аромат. Пицца, гамбургеры,
+картофель и мясные блюда пропитываются
+легким запахом дыма, их аромат
+становиться более насыщенным и
+выраженным.</p>
+
+<p>Предлагаем Вам настоящую итальянскую
+пиццу и гамбургеры с хрустящей
+корочкой, ароматные румяные шашлыки,
+люля-кебаб и блюда из картофеля.</p>
+</div>
+<center><h1 id="zagolovok">Наши товары:</h1></center>
+
+<div id="categoria">
+<img id="img-categoria" src="images/pizza.jpg" alt="Пицца"/>
+<center><a id="cat" href="/category.php">Пицца</a></center>
+</div>
+<div id="categoria">
+<img id="img-categoria" src="images/burgers.jpg" alt="Бургеры"/>
+<center><a id="cat" href="/category.php">Бургеры</a></center>
+</div>
+<div id="categoria">
+<img id="img-categoria" src="images/spila.jpg" alt="С пыла-жару"/>
+<center><a id="cat" href="/category.php">С пыла-жару</a></center>
+</div>
+<div id="categoria2">
+<img id="img-categoria" src="images/naugljah.jpg" alt="На углях"/>
+<center><a id="cat" href="/category.php">На Углях</a></center>
+</div>
+
+
+</div>
+<?php
+	include("include/block-footer.php");
+?>
+</div>
+</body>
+</html>
---
+++ spila.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" charset="utf-8"/>
+	<meta name="DemonApps" content="Demon" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+	<title>Блюда с пыла-жару</title>
+</head>
+
+<body>
+<div id="block-body">
+<?php
+	include("include/block-header.php");
+?>
+
+<div id="block-content">
+<div id="text">
+<h1 id="zagolovok">Блюда с пыла-жару</h1>
+<div>
+<img id="block-product" src="images/products/spila/free.jpg" alt="Картофель фри"/>
+</div>
+<img id="block-productimg-categoria" src="images/products/spila/po_derevensky.jpg" alt="Картофель по-деревенски"/>
+<img id="img-categoria" src="images/products/spila/kartofel_zapecheniy.jpg" alt="Картофель запеченый"/>
+<img id="img-categoria" src="images/products/spila/nagetsy.jpg" alt="Нагетсы"/>
+</div>
+<center><h1 id="zagolovok">Наши товары:</h1></center>
+
+<div id="categoria">
+<img id="img-categoria" src="images/pizza.jpg" alt="Пицца"/>
+<center><a id="cat" href="/category.php">Пицца</a></center>
+</div>
+<div id="categoria">
+<img id="img-categoria" src="images/burgers.jpg" alt="Бургеры"/>
+<center><a id="cat" href="/category.php">Бургеры</a></center>
+</div>
+<div id="categoria">
+<img id="img-categoria" src="images/spila.jpg" alt="С пыла-жару"/>
+<center><a id="cat" href="/category.php">С пыла-жару</a></center>
+</div>
+<div id="categoria2">
+<img id="img-categoria" src="images/naugljah.jpg" alt="На углях"/>
+<center><a id="cat" href="/category.php">На Углях</a></center>
+</div>
+
+
+</div>
+<?php
+	include("include/block-footer.php");
+?>
+</div>
+</body>
+</html>
