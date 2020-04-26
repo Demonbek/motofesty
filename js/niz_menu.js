40f7d5a50c0592c05e95c202bf8b1b4e16d287cf
Demonbek <34240090+Demonbek@users.noreply.github.com>
2020-04-26 05:38:51 +0300

    1

---
+++ js/niz_menu.js
@@ -0,0 +1,17 @@
+<center><h1 id="zagolovok">Наши товары:</h1></center>
+<div id="categoria">
+<img id="img-categoria" src="images/products/pizza/ohota.jpg" alt="Пицца"/>
+<center><a id="cat" href="/pizza.html">Пицца</a></center>
+</div>
+<div id="categoria">
+<img id="img-categoria" src="images/burgers.jpg" alt="Бургеры"/>
+<center><a id="cat" href="/burgery.html">Бургеры</a></center>
+</div>
+<div id="categoria">
+<img id="img-categoria" src="images/spila.jpg" alt="С пыла-жару"/>
+<center><a id="cat" href="/spila.html">С пыла-жару</a></center>
+</div>
+<div id="categoria2">
+<img id="img-categoria" src="images/naugljah.jpg" alt="На углях"/>
+<center><a id="cat" href="/naugljah.html" target=>На Углях</a></center>
+</div>
